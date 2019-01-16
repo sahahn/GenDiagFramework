3a3079a890a64b4a1e47de6cc9f21049650425ae
Sage Hahn <sahahn@uvm.edu>
2019-01-16 16:05:04 -0500

    By golly

--- scripts/IQ_test.py
+++ scripts/IQ_test.py
@@ -6,6 +6,8 @@ import numpy as np
 from DataLoaders.IQ_DataLoader import IQ_DataLoader
 from Generators.IQ_Generator import IQ_Generator
 from Models.Resnet3D import Resnet3DBuilder
+from sklearn.metrics import r2_score, mean_absolute_error, mean_squared_error
+
 
 
 input_dims = (256, 256, 256, 1)
@@ -48,6 +50,12 @@ train, test = dl.get_train_test_split(.2, 43)
 
 print(len(train), len(test))
 
+for t in train:
+    print(train.get_name(), train.get_label(copy=True))
+    
+for t in test:
+    print(test.get_name(), test.get_label(copy=True))
+
 rn_builder = Resnet3DBuilder()
 model = rn_builder.build_resnet_34(input_shape=input_dims, num_outputs=1, reg_factor=1e-4)
 model.compile(loss = 'mean_squared_error', optimizer = keras.optimizers.adam(.001))
@@ -58,6 +66,29 @@ model.fit_generator(generator=gen,
                     validation_data=test_gen,
                     use_multiprocessing=True,
                     workers=8,
-                    epochs=20)
+                    epochs=1)
+
+preds = model.predict_generator(test_gen)
+
+for p in len(preds):
+    test[p].set_pred_label(preds[p])
+    
+dl.reverse_label_scaling()
+
+true = [dp.get_label() for dp in test]
+pred = [dp.get_pred_label() for dp in test]
+
+print(true)
+print(pred)
+
+r2_score = r2_score(true, pred)
+print('r2 score: ', r2_score)
+
+mae_score = mean_absolute_error(true, pred)
+print('mae score: ', mae_score)
+
+mse_score = mean_squared_error(true, pred)
+print('mse score: ', mse_score)
+
                             
                             
