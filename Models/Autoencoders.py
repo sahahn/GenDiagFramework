49d2beded10db5c56111905d84ae55f003404554
sahahn <sahahn@uvm.edu>
2019-01-22 15:07:03 -0500

    new model?

--- Models/Autoencoders.py
+++ Models/Autoencoders.py
@@ -2,9 +2,9 @@
 import keras
 from keras.models import Sequential
 from keras.layers import Conv3D, MaxPooling3D, Deconvolution3D, LeakyReLU
-from keras.layers import Reshape, Dense, Flatten
+from keras.layers import Reshape, Dense, Flatten, Activation, BatchNormalization
 
-def encoder_model_200(input_shape=(160,192,160,1)):
+def encoder_model_200(input_shape=(160,192,160,1), bottleneck=200):
     
     enc = Sequential()
     
@@ -20,7 +20,7 @@ def encoder_model_200(input_shape=(160,192,160,1)):
     enc.add(LeakyReLU(alpha=.2))
     
     enc.add(Flatten())
-    enc.add(Dense(200, activation = 'sigmoid'))
+    enc.add(Dense(bottleneck, activation = 'sigmoid'))
     
     enc.add(Dense(256*5*6*5))
     enc.add(Reshape((5,6,5,256)))
@@ -57,3 +57,65 @@ def brain_enc_model(input_shape=(160,192,160,1)):
     
     enc.summary()
     return enc
+
+
+def CNN_3D_AE(input_shape=(160,192,160,1), bottleneck=200):
+    filters = [4, 8, 16, 32, 64]
+    model = Sequential()
+
+    model.add(Conv3D(filters=filters[0], kernel_size=(3, 3, 3), padding='same', input_shape=input_shape))
+    model.add(Activation('relu'))
+    model.add(Conv3D(filters=filters[0], kernel_size=(3, 3, 3), padding='same'))
+    model.add(BatchNormalization(axis=-1))
+    model.add(Activation('relu'))
+    model.add(MaxPooling3D(pool_size=(2, 2, 2), strides=2, padding='valid'))
+    
+    model.add(Conv3D(filters=filters[1], kernel_size=(3, 3, 3), padding='same'))
+    model.add(Activation('relu'))
+    model.add(Conv3D(filters=filters[1], kernel_size=(3, 3, 3), padding='same'))
+    model.add(BatchNormalization(axis=-1))
+    model.add(Activation('relu'))
+    model.add(MaxPooling3D(pool_size=(2, 2, 2), strides=2, padding='valid'))
+              
+    model.add(Conv3D(filters=filters[2], kernel_size=(3, 3, 3), padding='same'))
+    model.add(Activation('relu'))
+    model.add(Conv3D(filters=filters[2], kernel_size=(3, 3, 3), padding='same'))
+    model.add(BatchNormalization(axis=-1))
+    model.add(Activation('relu'))
+    model.add(MaxPooling3D(pool_size=(2, 2, 2), strides=2, padding='valid'))
+    
+    model.add(Conv3D(filters=filters[3], kernel_size=(3, 3, 3), padding='same'))
+    model.add(Activation('relu'))
+    model.add(Conv3D(filters=filters[3], kernel_size=(3, 3, 3), padding='same'))
+    model.add(BatchNormalization(axis=-1))
+    model.add(Activation('relu'))
+    model.add(MaxPooling3D(pool_size=(2, 2, 2), strides=2, padding='valid'))
+    
+    model.add(Conv3D(filters=filters[4], kernel_size=(3, 3, 3), padding='same'))
+    model.add(Activation('relu'))
+    model.add(Conv3D(filters=filters[4], kernel_size=(3, 3, 3), padding='same'))
+    model.add(BatchNormalization(axis=-1))
+    model.add(Activation('relu'))
+    model.add(MaxPooling3D(pool_size=(2, 2, 2), strides=2, padding='valid'))
+              
+    model.add(Flatten())
+    model.add(Dense(bottleneck, activation = 'sigmoid'))
+    model.add(Dense(64*5*6*5))
+    model.add(Reshape((5,6,5,64)))
+    
+
+    model.add(Deconvolution3D(filters=64, kernel_size=3, padding='same',
+                            strides=2, activation='relu'))
+    model.add(Deconvolution3D(filters=32, kernel_size=3, padding='same',
+                            strides=2, activation='relu'))
+    model.add(Deconvolution3D(filters=16, kernel_size=3, padding='same',
+                            strides=2, activation='relu'))
+    model.add(Deconvolution3D(filters=8, kernel_size=3, padding='same',
+                            strides=2, activation='relu'))
+    model.add(Deconvolution3D(filters=1, kernel_size=3, padding='same',
+                            strides=2, activation='relu'))    
+    
+    model.summary()
+    return model
+
+CNN_3D_AE()
