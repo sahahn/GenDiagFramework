cf355884149887a1bf17aef3cb0687cc0a372841
sahahn <sahahn@uvm.edu>
2019-03-04 13:01:19 -0500

    saf

--- DataUtils/crop_tools.py
+++ DataUtils/crop_tools.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 import numpy as np
-import nilearn
 
 def get_crop_ind(image, rtol= 1e-08):
 
@@ -19,8 +18,8 @@ def fill_to(data, input_size):
     if (shp[:-1] < np.array(input_size)[:-1]).all():
         new_data = np.zeros(input_size)
 
-        d1 = np.floor((np.shape(new_data) - np.shape(data))/2)
-        d2 = np.ceil((np.shape(new_data) - np.shape(data))/2)
+        d1 = np.floor(np.array((np.shape(new_data)) - np.array(np.shape(data)))/2)
+        d2 = np.ceil(np.array((np.shape(new_data)) - np.array(np.shape(data)))/2)
         
         new_data[d1[0]:shp[0]+d2[0], d1[1]:shp[1]+d2[1], d1[2]:shp[2]+d2[2]] = data
         
